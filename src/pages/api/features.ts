263beca1fc77b961277e1789b127587406cf64c9
Geir Gåsodden <geir.gasodden@gmail.com>
2023-08-09 10:35:20 +0200

    r henter toggles fra unleash-next

--- src/pages/api/features.ts
+++ src/pages/api/features.ts
@@ -1,13 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { logger } from '@navikt/next-logger';
+import { getDefinitions } from '@unleash/nextjs';
 
 async function features(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const url = `${process.env.FEATURE_TOGGLE_URL}`;
-        const response = await fetch(url);
-        const json = await response.json();
-        const features = json.features || [];
-        return res.status(200).json(features);
+        const definitions = await getDefinitions();
+        return res.status(200).json(definitions.features || []);
     } catch (error) {
         logger.error(error);
         return res.status(200).json([]);
