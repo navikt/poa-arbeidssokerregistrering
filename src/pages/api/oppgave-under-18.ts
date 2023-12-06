603358c036eed728b98e017c965cd3d5a811eb3f
Jostein Holje <jostein.holje@nav.no>
2023-12-06 10:02:46 +0100

    Oppgave-under-18 med token-x utveksling

--- src/pages/api/oppgave-under-18.ts
+++ src/pages/api/oppgave-under-18.ts
@@ -1,7 +1,7 @@
-import { lagApiPostHandlerMedAuthHeaders } from '../../lib/next-api-handler';
+import { AIA_BACKEND_CLIENT_ID, lagApiPostHandlerMedAuthHeaders } from '../../lib/next-api-handler';
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 
 const opprettOppgaveUrl = `${process.env.OPPRETT_OPPGAVE_URL}/under-18`;
-const opprettOppgaveHandler = lagApiPostHandlerMedAuthHeaders(opprettOppgaveUrl);
+const opprettOppgaveHandler = lagApiPostHandlerMedAuthHeaders(opprettOppgaveUrl, undefined, AIA_BACKEND_CLIENT_ID);
 
 export default withAuthenticatedApi(opprettOppgaveHandler);
