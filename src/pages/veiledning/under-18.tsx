2be902fb13585b2e7736e29e584f54c5eacfdfbc
Geir Gåsodden <geir.gasodden@gmail.com>
2023-12-13 08:18:49 +0100

    r oppretter oppgave automatisk for de som blir stoppet pga alder

--- src/pages/veiledning/under-18.tsx
+++ src/pages/veiledning/under-18.tsx
@@ -1,12 +1,9 @@
-import { useCallback, useState } from 'react';
-import { BodyLong, GuidePanel, Heading, Button } from '@navikt/ds-react';
+import { useCallback, useState, useEffect } from 'react';
+import { Loader } from '@navikt/ds-react';
 import { preload } from 'swr';
-import { useRouter } from 'next/router';
 
-import useSprak from '../../hooks/useSprak';
 import { useConfig } from '../../contexts/config-context';
 
-import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
 import { loggAktivitet } from '../../lib/amplitude';
 import { Config } from '../../model/config';
 
@@ -18,24 +15,10 @@ import {
 import { fetcher, fetcher as api } from '../../lib/api-utils';
 import { withAuthenticatedPage } from '../../auth/withAuthentication';
 
-const TEKSTER: Tekster<string> = {
-    nb: {
-        overskrift: 'Du trenger samtykke for å registrere deg',
-        samtykke: 'Personer under 18 år trenger samtykke for å registrere seg som arbeidssøker.',
-        sorry: 'Du kan derfor ikke registrere deg på nett.',
-        sendMelding: 'Send oss beskjed om at du ønsker å registrere deg, så vil en veileder ta kontakt.',
-        veileder:
-            'Veilederen vil hjelpe deg med registreringen og gi mer informasjon om hvilke tjenester NAV kan tilby deg.',
-        knappeTekst: 'Jeg ønsker å bli kontaktet av en veileder',
-        avbryt: 'Avbryt',
-    },
-};
-
 function Under18() {
-    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
     const [responseMottatt, settResponseMottatt] = useState<boolean>(false);
+    const [oppretterOppgave, settOppretterOppgave] = useState<boolean>(false);
     const [feil, settFeil] = useState<Opprettelsesfeil | undefined>(undefined);
-    const Router = useRouter();
     const { enableMock } = useConfig() as Config;
     const brukerMock = enableMock === 'enabled';
     preload('api/kontaktinformasjon/', fetcher);
@@ -67,34 +50,20 @@ function Under18() {
         settResponseMottatt(true);
     }, []);
 
-    const avbrytKontaktMeg = () => {
-        loggAktivitet({ aktivitet: 'Avbryter kontakt meg - under 18' });
-        Router.push('/');
-    };
+    useEffect(() => {
+        if (!oppretterOppgave) {
+            settOppretterOppgave(true);
+            opprettOppgave();
+        }
+    }, [oppretterOppgave]);
 
     if (responseMottatt) {
         return feil ? <KvitteringOppgaveIkkeOpprettet feil={feil} /> : <KvitteringOppgaveOpprettet />;
     } else
         return (
-            <>
-                <GuidePanel poster>
-                    <Heading spacing size="large" level="1">
-                        {tekst('overskrift')}
-                    </Heading>
-                    <BodyLong>{tekst('samtykke')}</BodyLong>
-                    <BodyLong>{tekst('sorry')}</BodyLong>
-                    <BodyLong>{tekst('sendMelding')}</BodyLong>
-                    <BodyLong>{tekst('veileder')}</BodyLong>
-                </GuidePanel>
-                <section className="flex-center mhl">
-                    <Button onClick={opprettOppgave} className="mrl">
-                        {tekst('knappeTekst')}
-                    </Button>
-                    <Button variant="tertiary" onClick={avbrytKontaktMeg}>
-                        {tekst('avbryt')}
-                    </Button>
-                </section>
-            </>
+            <div>
+                <Loader size="xlarge" />
+            </div>
         );
 }
 
