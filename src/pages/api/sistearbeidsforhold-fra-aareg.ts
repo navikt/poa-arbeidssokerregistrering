0cc999b7b6600fe25ed3166dee0ab53338c426f2
Jostein Holje <jostein.holje@nav.no>
2024-01-10 13:13:14 +0100

    Debug: returner response fra aareg

--- src/pages/api/sistearbeidsforhold-fra-aareg.ts
+++ src/pages/api/sistearbeidsforhold-fra-aareg.ts
@@ -5,7 +5,6 @@ import { decodeJwt } from 'jose';
 import { getAaregToken, getHeaders, getTokenFromRequest } from '../../lib/next-api-handler';
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 import { verifyToken } from '../../auth/token-validation';
-import { hentSisteArbeidsForhold } from '../../lib/hent-siste-arbeidsforhold';
 import { logger } from '@navikt/next-logger';
 
 const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
@@ -45,24 +44,24 @@ async function hentFraAareg(req: NextApiRequest, callId: string) {
         },
     );
     logger.info(`Kall callId: ${callId} mot ${url} er ferdig`);
-    logger.info(arbeidsforholdoversikt, 'Arbeidsforholdoversikt');
-    logger.info({ arbeidsforholdoversikt, msg: 'Arbeidsforholdoversikt' });
-    return hentSisteArbeidsForhold(arbeidsforholdoversikt);
+    return arbeidsforholdoversikt;
+    // return hentSisteArbeidsForhold(arbeidsforholdoversikt);
 }
 
 const sisteArbeidsforhold = async (req: NextApiRequest, res: NextApiResponse<any>) => {
     const callId = nanoid();
 
     try {
-        const { styrk } = await hentFraAareg(req, callId);
-        const { konseptMedStyrk08List } = await fetch(
-            `${process.env.PAM_JANZZ_URL}/kryssklassifiserMedKonsept?kodeForOversetting=${styrk}`,
-            {
-                headers: getHeaders('token', callId),
-            },
-        ).then((res) => res.json());
-
-        res.json(konseptMedStyrk08List[0]);
+        res.json(await hentFraAareg(req, callId));
+        // const { styrk } = await hentFraAareg(req, callId);
+        //     const { konseptMedStyrk08List } = await fetch(
+        //         `${process.env.PAM_JANZZ_URL}/kryssklassifiserMedKonsept?kodeForOversetting=${styrk}`,
+        //         {
+        //             headers: getHeaders('token', callId),
+        //         },
+        //     ).then((res) => res.json());
+        //
+        //     res.json(konseptMedStyrk08List[0]);
     } catch (e) {
         logger.error(`Feil ved henting av siste arbeidsforhold fra aareg [callId: ${callId}]`, e);
         res.status(500).end(`${e}`);
