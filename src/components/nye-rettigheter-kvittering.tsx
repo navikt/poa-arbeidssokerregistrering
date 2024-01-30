4604f0be4fa724af49ccc905e1021b08990f533c
Geir Gåsodden <geir.gasodden@gmail.com>
2024-01-30 08:02:27 +0100

    r legger inn resten av teksten på kvittering uten plikter

--- src/components/nye-rettigheter-kvittering.tsx
+++ src/components/nye-rettigheter-kvittering.tsx
@@ -29,19 +29,19 @@ const NyeRettigheterKvittering = () => {
             <Box className="mb-12">
                 <List as="ul">
                     <List.Item>
-                        Etter at du har registrert deg vil opplysningene du har gitt om utdanning, livssituasjon og
-                        tidligere arbeidsforhold bli brukt til å vurdere hvilken hjelp du skal få fra NAV. Vi vurderer
-                        de opplysningene du har gitt oss opp mot de opplysningene vi har om andre arbeidssøkere i
-                        omtrent samme situasjon som deg. På bakgrunn av dette vil en veileder fatte et vedtak som sendes
-                        til deg. Vedtaket forteller hvordan NAV vurderer din situasjon i arbeidsmarkedet.
+                        Nå vil opplysningene du har gitt om utdanning, livssituasjon og tidligere arbeidsforhold bli
+                        brukt til å vurdere hvilken hjelp du skal få fra NAV. Vi vurderer de opplysningene du har gitt
+                        oss opp mot de opplysningene vi har om andre arbeidssøkere i omtrent samme situasjon som deg. På
+                        bakgrunn av dette vil en veileder fatte et vedtak som sendes til deg. Vedtaket forteller hvordan
+                        NAV vurderer din situasjon i arbeidsmarkedet.
                     </List.Item>
                     <List.Item>
                         Dersom du er uenig i NAV sin vurdering, har du mulighet til å gi tilbakemelding om dette inne på
                         innloggede sider.
                     </List.Item>
                     <List.Item>
-                        Avhengig av hvilken hjelp og ytelser du har krav på kan du få ulike plikter som NAV forventer at
-                        du følger opp.
+                        Om du søker om pengestøtte fra NAV vil du få mer informasjon om hva NAV krever av deg dersom du
+                        får denne innvilget.
                     </List.Item>
                     <List.Item>
                         I den perioden du ønsker å være registrert som arbeidssøker hos NAV er det viktig at du leverer
@@ -51,6 +51,10 @@ const NyeRettigheterKvittering = () => {
                         Hvis det skjer endringer i livet ditt som påvirker din status som arbeidssøker må du ta kontakt
                         med NAV. Da gjør vi en ny vurdering av ditt behov.
                     </List.Item>
+                </List>
+                <List as="ul" title="For å komme deg raskest mulig tilbake i jobb anbefaler vi også at:">
+                    <List.Item>du oppdaterer CVen din</List.Item>
+                    <List.Item>holder deg oppdatert på arbeidsmarkedet innenfor ditt felt</List.Item>
                 </List>
             </Box>
         </div>
