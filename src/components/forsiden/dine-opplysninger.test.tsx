3c60a715390a2f3775b0ad2bc203320b30beb243
Geir Gåsodden <geir.gasodden@gmail.com>
2024-02-14 07:28:24 +0100

    r legger til testing med jest-axe

--- src/components/forsiden/dine-opplysninger.test.tsx
+++ src/components/forsiden/dine-opplysninger.test.tsx
@@ -1,11 +1,19 @@
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 
 jest.mock('next/router', () => require('next-router-mock'));
+expect.extend(toHaveNoViolations);
 import DineOpplysninger from './dine-opplysninger';
 
 describe('tester komponenten DineOpplysninger', () => {
     test('komponenten rendrer som forventet', () => {
         const { container } = render(<DineOpplysninger />);
         expect(container).not.toBeEmptyDOMElement();
+    });
+
+    test('sjekker komponenten DineOpplysninger med axe', async () => {
+        const { container } = render(<DineOpplysninger />);
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
     });
 });
