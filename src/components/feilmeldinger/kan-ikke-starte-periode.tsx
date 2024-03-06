608c08dad6cca948e1fda43f5c6afa793c6863ed
Geir Gåsodden <geir.gasodden@gmail.com>
2024-03-06 08:53:00 +0100

    r vidersender de under 18 år til opprinnelig feilside

--- src/components/feilmeldinger/kan-ikke-starte-periode.tsx
+++ src/components/feilmeldinger/kan-ikke-starte-periode.tsx
@@ -1,12 +1,21 @@
 import { Alert, BodyLong, GuidePanel } from '@navikt/ds-react';
+import { useRouter } from 'next/router';
 
 import { FeilmeldingVedStartAvArbeidssoekerperiode } from '../../model/feilsituasjonTyper';
 
 function KanIkkeStartePeriode(props: { feilmelding?: FeilmeldingVedStartAvArbeidssoekerperiode }) {
     const { feilmelding } = props;
+    const Router = useRouter();
     if (!feilmelding) return null;
     const { feilKode, aarsakTilAvvisning } = feilmelding;
     const { regel, detaljer } = aarsakTilAvvisning || {};
+
+    const erUnder18 = regel === 'UNDER_18_AAR';
+
+    if (erUnder18) {
+        Router.push('/veiledning/under-18/');
+    }
+
     return (
         <GuidePanel poster>
             <Alert variant="error" className="mb-6">
