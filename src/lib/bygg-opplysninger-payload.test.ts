fc01a15f9921d18348cbcc4ea59b3e8dc5f9707c
Jostein Holje <jostein.holje@nav.no>
2024-03-06 15:02:45 +0100

    lib/bygg-opplysninger-payload

---
+++ src/lib/bygg-opplysninger-payload.test.ts
@@ -0,0 +1,105 @@
+import byggOpplysningerPayload from './bygg-opplysninger-payload';
+import {
+    DinSituasjon,
+    JaEllerNei,
+    SisteStillingValg,
+    SporsmalId,
+    UtdanningGodkjentValg,
+    Utdanningsnivaa,
+} from '@navikt/arbeidssokerregisteret-utils';
+
+describe('bygg-opplysninger-payload', () => {
+    describe('utdanning', () => {
+        test('mapper til nus-kode, godkjent, bestatt', () => {
+            const result = byggOpplysningerPayload({
+                [SporsmalId.utdanning]: Utdanningsnivaa.HOYERE_UTDANNING_1_TIL_4,
+                [SporsmalId.utdanningBestatt]: JaEllerNei.JA,
+                [SporsmalId.utdanningGodkjent]: UtdanningGodkjentValg.VET_IKKE,
+            });
+            expect(result.utdanning).toEqual({
+                nus: '6',
+                bestatt: 'JA',
+                godkjent: 'VET_IKKE',
+            });
+        });
+        test('dropper godkjent og bestatt når ingen utdanning', () => {
+            const result = byggOpplysningerPayload({
+                [SporsmalId.utdanning]: Utdanningsnivaa.INGEN_UTDANNING,
+                [SporsmalId.utdanningBestatt]: JaEllerNei.JA,
+                [SporsmalId.utdanningGodkjent]: UtdanningGodkjentValg.VET_IKKE,
+            });
+            expect(result.utdanning).toEqual({
+                nus: '0',
+            });
+        });
+        test('defaulter til nus-kode ingen svar', () => {
+            const result = byggOpplysningerPayload({});
+            expect(result.utdanning).toEqual({
+                nus: '9',
+            });
+        });
+    });
+
+    describe('jobbsituasjon', () => {
+        test('dropper detaljer ved HAR_IKKE_HATT_JOBB', () => {
+            const result = byggOpplysningerPayload({
+                [SporsmalId.dinSituasjon]: DinSituasjon.ALDRI_HATT_JOBB,
+            });
+            expect(result.jobbsituasjon).toEqual({
+                beskrivelser: [{ beskrivelse: 'ALDRI_HATT_JOBB' }],
+            });
+        });
+        test('dropper detaljer ved ALDRI_HATT_JOBB', () => {
+            const result = byggOpplysningerPayload({
+                [SporsmalId.dinSituasjon]: DinSituasjon.AKKURAT_FULLFORT_UTDANNING,
+                [SporsmalId.sisteStilling]: SisteStillingValg.HAR_IKKE_HATT_JOBB,
+            });
+            expect(result.jobbsituasjon).toEqual({
+                beskrivelser: [{ beskrivelse: 'AKKURAT_FULLFORT_UTDANNING' }],
+            });
+        });
+        test('legger med styrk-kode i detaljer', () => {
+            const result = byggOpplysningerPayload({
+                [SporsmalId.dinSituasjon]: DinSituasjon.VIL_FORTSETTE_I_JOBB,
+                [SporsmalId.sisteJobb]: {
+                    styrk08: '42',
+                    label: 'Bartender',
+                    konseptId: 0,
+                },
+            });
+            expect(result.jobbsituasjon).toEqual({
+                beskrivelser: [
+                    {
+                        beskrivelse: 'VIL_FORTSETTE_I_JOBB',
+                        detaljer: {
+                            stilling: 'Bartender',
+                            stilling_styrk08: '42',
+                        },
+                    },
+                ],
+            });
+        });
+    });
+
+    test('mapper skjema-state til payload', () => {
+        const result = byggOpplysningerPayload({
+            [SporsmalId.utdanning]: Utdanningsnivaa.INGEN_UTDANNING,
+            [SporsmalId.utdanningBestatt]: JaEllerNei.JA,
+            [SporsmalId.utdanningGodkjent]: UtdanningGodkjentValg.VET_IKKE,
+            [SporsmalId.helseHinder]: JaEllerNei.NEI,
+            [SporsmalId.dinSituasjon]: DinSituasjon.ALDRI_HATT_JOBB,
+        });
+
+        expect(result).toEqual({
+            utdanning: {
+                nus: '0',
+            },
+            helse: {
+                helsetilstandHindrerArbeid: 'NEI',
+            },
+            jobbsituasjon: {
+                beskrivelser: [{ beskrivelse: 'ALDRI_HATT_JOBB' }],
+            },
+        });
+    });
+});
---
+++ src/lib/bygg-opplysninger-payload.ts
@@ -0,0 +1,111 @@
+import { SkjemaState } from '../model/skjema';
+import {
+    DinSituasjon,
+    JaEllerNei,
+    SisteStillingValg,
+    SporsmalId,
+    UtdanningGodkjentValg,
+    Utdanningsnivaa,
+} from '@navikt/arbeidssokerregisteret-utils';
+
+// temp - eksporter i utils
+export enum NUS {
+    INGEN_UTDANNING = '0',
+    GRUNNSKOLE = '2',
+    VIDEREGAENDE_GRUNNUTDANNING = '3',
+    VIDEREGAENDE_FAGBREV_SVENNEBREV = '4',
+    VIDEREGAENDE_PAABYGGING = '5',
+    HOYERE_UTDANNING_1_TIL_4 = '6',
+    HOYERE_UTDANNING_5_ELLER_MER = '7',
+    INGEN_SVAR = '9',
+}
+
+type Payload = {
+    utdanning: {
+        nus: NUS;
+        bestatt?: JaEllerNei;
+        godkjent?: UtdanningGodkjentValg;
+    };
+    helse: {
+        helsetilstandHindrerArbeid: JaEllerNei;
+    };
+    jobbsituasjon: {
+        beskrivelser: [{ beskrivelse: DinSituasjon; detaljer?: any }];
+    };
+    annet: {
+        andreForholdHindrerArbeid: JaEllerNei;
+    };
+};
+
+function mapUtdanningsnivaaTilNusKode(utdanning?: Utdanningsnivaa): NUS {
+    switch (utdanning) {
+        case Utdanningsnivaa.INGEN_UTDANNING:
+            return NUS.INGEN_UTDANNING;
+        case Utdanningsnivaa.GRUNNSKOLE:
+            return NUS.GRUNNSKOLE;
+        case Utdanningsnivaa.VIDEREGAENDE_GRUNNUTDANNING:
+            return NUS.VIDEREGAENDE_GRUNNUTDANNING;
+        case Utdanningsnivaa.VIDEREGAENDE_FAGBREV_SVENNEBREV:
+            return NUS.VIDEREGAENDE_FAGBREV_SVENNEBREV;
+        case Utdanningsnivaa.HOYERE_UTDANNING_1_TIL_4:
+            return NUS.HOYERE_UTDANNING_1_TIL_4;
+        case Utdanningsnivaa.HOYERE_UTDANNING_5_ELLER_MER:
+            return NUS.HOYERE_UTDANNING_5_ELLER_MER;
+        case Utdanningsnivaa.INGEN_SVAR:
+            return NUS.INGEN_SVAR;
+        default:
+            return NUS.INGEN_SVAR;
+    }
+}
+function mapUtdanning(skjema: SkjemaState): Payload['utdanning'] {
+    const nus = mapUtdanningsnivaaTilNusKode(skjema[SporsmalId.utdanning]);
+    if ([NUS.INGEN_SVAR, NUS.INGEN_UTDANNING].includes(nus)) {
+        return {
+            nus,
+        };
+    }
+
+    return {
+        nus,
+        bestatt: skjema[SporsmalId.utdanningBestatt],
+        godkjent: skjema[SporsmalId.utdanningGodkjent],
+    };
+}
+
+function mapJobbsituasjon(skjema: SkjemaState): Payload['jobbsituasjon'] {
+    const harAldriJobbet =
+        skjema.dinSituasjon === DinSituasjon.ALDRI_HATT_JOBB ||
+        skjema.sisteStilling === SisteStillingValg.HAR_IKKE_HATT_JOBB;
+
+    return {
+        beskrivelser: [
+            {
+                beskrivelse: skjema[SporsmalId.dinSituasjon]!,
+                detaljer: harAldriJobbet
+                    ? undefined
+                    : {
+                          stilling: skjema[SporsmalId.sisteJobb]?.label,
+                          stilling_styrk08: skjema[SporsmalId.sisteJobb]?.styrk08,
+                      },
+            },
+        ],
+    };
+}
+function byggOpplysningerPayload(skjemaState: SkjemaState) {
+    return {
+        utdanning: mapUtdanning(skjemaState),
+        helse: skjemaState['helseHinder']
+            ? {
+                  helsetilstandHindrerArbeid: skjemaState['helseHinder'],
+              }
+            : undefined,
+        jobbsituasjon: mapJobbsituasjon(skjemaState),
+        annet: skjemaState['andreForhold']
+            ? {
+                  andreForholdHindrerArbeid: skjemaState['andreForhold'],
+              }
+            : undefined,
+    };
+}
+
+export default byggOpplysningerPayload;
