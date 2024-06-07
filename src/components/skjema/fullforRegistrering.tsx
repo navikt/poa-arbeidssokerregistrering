f5facc96491ddd47740d9c4ba66264cdd6d6770d
Jostein Holje <jostein.holje@nav.no>
2024-06-07 13:44:31 +0200

    Fjern siste rest av 'bruk-ny-inngang' toggle

--- src/components/skjema/fullforRegistrering.tsx
+++ src/components/skjema/fullforRegistrering.tsx
@@ -1,26 +1,13 @@
-import { useCallback, useState } from 'react';
-import { Alert, Button, ConfirmationPanel, GuidePanel, Heading, ReadMore } from '@navikt/ds-react';
-import { useRouter } from 'next/router';
-import { logger } from '@navikt/next-logger';
+import { useState } from 'react';
+import { Alert, ConfirmationPanel, GuidePanel, Heading, ReadMore } from '@navikt/ds-react';
 import Head from 'next/head';
 
 import useSprak from '../../hooks/useSprak';
-import { useConfig } from '../../contexts/config-context';
-import { useFeatureToggles } from '../../contexts/featuretoggle-context';
 
-import { DinSituasjon, lagHentTekstForSprak, SporsmalId, Tekster } from '@navikt/arbeidssokerregisteret-utils';
+import { lagHentTekstForSprak, Tekster } from '@navikt/arbeidssokerregisteret-utils';
 import { Side, SkjemaState } from '../../model/skjema';
-import { fetcher as api } from '../../lib/api-utils';
-import byggFullforRegistreringPayload from '../../lib/bygg-fullfor-registrering-payload';
-import { FeilmeldingGenerell } from '../feilmeldinger/feilmeldinger';
-import { FullforRegistreringResponse, Innsatsgruppe } from '../../model/registrering';
-import hentKvitteringsUrl from '../../lib/hent-kvitterings-url';
-import { loggAktivitet, loggEksperiment, loggFlyt } from '../../lib/amplitude';
 import guidePanelStyles from '../../styles/guidepanel.module.css';
 import PlikterSvg from '../forsiden/plikter-svg';
-import { Config } from '../../model/config';
-import { hentRegistreringFeiletUrl } from '../../lib/hent-registrering-feilet-url';
-import { OppgaveRegistreringstype } from '../../model/feilsituasjonTyper';
 import FullforRegistreringKnappNyInngang from './fullfor-registrering-knapp-ny-inngang';
 
 const TEKSTER: Tekster<string> = {
@@ -60,133 +47,11 @@ interface FullforProps {
     onSubmit(): void;
 }
 
-const beregnTidBrukt = (skjemaState: SkjemaState) => {
-    if (!skjemaState.startTid) {
-        return;
-    }
-
-    return (Date.now() - skjemaState.startTid) / 1000;
-};
-
-const hentProfilering = async (response: FullforRegistreringResponse, side: Side) => {
-    if (!response.type && side === 'standard') {
-        try {
-            return await api('api/profilering/');
-        } catch (e) {
-            logger.error('profilering feilet', e);
-        }
-    }
-};
-
-interface FullforKnappProps extends FullforProps {
-    onValiderSkjema(): boolean;
-}
-
-export const FullforRegistreringKnapp = (props: FullforKnappProps) => {
-    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
-    const [senderSkjema, settSenderSkjema] = useState<boolean>(false);
-    const [visFeilmelding, settVisFeilmelding] = useState<boolean>(false);
-    const [visFeilmeldingTeller, settVisFeilmeldingTeller] = useState<number>(0);
-    const router = useRouter();
-    const { toggles } = useFeatureToggles();
-    const { dittNavUrl } = useConfig() as Config;
-
-    const { skjemaState, onSubmit, onValiderSkjema } = props;
-
-    const validerOgFullfor = () => {
-        if (onValiderSkjema()) {
-            return fullforRegistrering();
-        }
-    };
-
-    const fullforRegistrering = useCallback(async () => {
-        try {
-            const body = byggFullforRegistreringPayload(skjemaState, props.side);
-            settSenderSkjema(true);
-            settVisFeilmelding(false);
-            onSubmit();
-
-            const response: FullforRegistreringResponse = await api('api/fullforregistrering', {
-                method: 'post',
-                body: JSON.stringify(body),
-            });
-
-            const feiltype = response.type;
-
-            if (feiltype) {
-                loggFlyt({ hendelse: 'Får ikke fullført registreringen', aarsak: feiltype });
-                return router.push(hentRegistreringFeiletUrl(feiltype, OppgaveRegistreringstype.REGISTRERING));
-            }
-
-            const profilering = await hentProfilering(response, props.side);
-            const dinSituasjon = skjemaState[SporsmalId.dinSituasjon];
-            const erStandardInnsatsgruppe = profilering && profilering.innsatsgruppe === Innsatsgruppe.STANDARD_INNSATS;
-            const harMistetJobbSagtOppEllerPermittert =
-                dinSituasjon &&
-                [DinSituasjon.MISTET_JOBBEN, DinSituasjon.ER_PERMITTERT, DinSituasjon.HAR_SAGT_OPP].includes(
-                    dinSituasjon,
-                );
-
-            const skalHoppeOverKvittering =
-                erStandardInnsatsgruppe &&
-                harMistetJobbSagtOppEllerPermittert &&
-                toggles['arbeidssokerregistrering.eksperimenter.videresend-til-aia'];
-
-            loggAktivitet({
-                aktivitet: 'Utfylling av skjema fullført',
-                tidBruktForAaFullforeSkjema: beregnTidBrukt(skjemaState),
-                innsatsgruppe: profilering ? profilering.innsatsgruppe : 'IKKE_PROFILERT',
-            });
-
-            loggFlyt({ hendelse: 'Sender inn skjema for registrering' });
-
-            if (skalHoppeOverKvittering) {
-                loggEksperiment({
-                    eksperiment: 'Videresender til AiA',
-                    innsatsgruppe: profilering.innsatsgruppe,
-                    situasjon: dinSituasjon,
-                });
-                loggFlyt({ hendelse: 'Registrering fullført' });
-
-                return router.push(`${dittNavUrl}?goTo=registrering&aia.aktivTab=ytelse`);
-            }
-            return router.push(hentKvitteringsUrl(props.side));
-        } catch (e) {
-            settVisFeilmeldingTeller(visFeilmeldingTeller + 1);
-            settVisFeilmelding(true);
-            logger.error(e, `Registrering (${props.side}) feilet`);
-            if (visFeilmeldingTeller >= 3) {
-                return router.push('/feil/');
-            }
-        } finally {
-            settSenderSkjema(false);
-        }
-    }, [onSubmit, props.side, router, skjemaState, visFeilmeldingTeller]);
-
-    return (
-        <>
-            {visFeilmelding && (
-                <div className="mb-6">
-                    <FeilmeldingGenerell />
-                </div>
-            )}
-            <div style={{ textAlign: 'center' }}>
-                <Button onClick={validerOgFullfor} loading={senderSkjema} disabled={senderSkjema}>
-                    {tekst('fullfoerRegistrering')}
-                </Button>
-            </div>
-        </>
-    );
-};
-
 const FullforRegistrering = (props: FullforProps) => {
     const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
     const [lestKravChecked, setLestKravChecked] = useState<boolean>(false);
     const [visFeilmeldingLestKrav, settVisFeilmeldingLestKrav] = useState<boolean>(false);
     const { skjemaState, onSubmit, side } = props;
-
-    const { toggles } = useFeatureToggles();
-    const brukNyInngang = toggles['arbeidssokerregistrering.bruk-ny-inngang'];
 
     const onValiderSkjema = () => {
         if (!lestKravChecked) {
@@ -261,21 +126,12 @@ const FullforRegistrering = (props: FullforProps) => {
                     </div>
                 )}
 
-                {brukNyInngang ? (
-                    <FullforRegistreringKnappNyInngang
-                        skjemaState={skjemaState}
-                        onSubmit={onSubmit}
-                        onValiderSkjema={onValiderSkjema}
-                        tekst={tekst}
-                    />
-                ) : (
-                    <FullforRegistreringKnapp
-                        skjemaState={skjemaState}
-                        side={side}
-                        onSubmit={onSubmit}
-                        onValiderSkjema={onValiderSkjema}
-                    />
-                )}
+                <FullforRegistreringKnappNyInngang
+                    skjemaState={skjemaState}
+                    onSubmit={onSubmit}
+                    onValiderSkjema={onValiderSkjema}
+                    tekst={tekst}
+                />
             </div>
         </>
     );
