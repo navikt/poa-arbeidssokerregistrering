6a3a32a6f03b4954868e18ea4d030241d79e5694
Jostein Holje <jostein.holje@nav.no>
2024-06-11 08:38:19 +0200

    Fjern /api/arbeidssoker

--- src/pages/api/arbeidssoker.ts
+++
@@ -1,8 +0,0 @@
-import lagApiHandlerMedAuthHeaders from '../../lib/next-api-handler';
-import { withAuthenticatedApi } from '../../auth/withAuthentication';
-
-const arbeidssokerUrl = `${process.env.ARBEIDSSOKER_PERIODER_URL}?fraOgMed=2020-01-01`;
-
-const arbeidssokerHandler = lagApiHandlerMedAuthHeaders(arbeidssokerUrl);
-
-export default withAuthenticatedApi(arbeidssokerHandler);
