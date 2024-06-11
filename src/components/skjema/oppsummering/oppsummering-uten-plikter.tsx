fa245b16073195f5e964a88d5dae6dbb961c3975
Jostein Holje <jostein.holje@nav.no>
2024-06-11 08:57:37 +0200

    Fjern /api/profilering

--- src/components/skjema/oppsummering/oppsummering-uten-plikter.tsx
+++ src/components/skjema/oppsummering/oppsummering-uten-plikter.tsx
@@ -1,7 +1,4 @@
-import { useCallback, useState } from 'react';
-import { BodyLong, Button, GuidePanel, Heading } from '@navikt/ds-react';
-import { useRouter } from 'next/router';
-import { logger } from '@navikt/next-logger';
+import { BodyLong, GuidePanel, Heading } from '@navikt/ds-react';
 import Head from 'next/head';
 // import useSWR from 'swr';
 import { lagHentTekstForSprak, Tekster } from '@navikt/arbeidssokerregisteret-utils';
@@ -9,14 +6,6 @@ import { lagHentTekstForSprak, Tekster } from '@navikt/arbeidssokerregisteret-ut
 import useSprak from '../../../hooks/useSprak';
 import OppsummeringSvg from './oppsummering-svg';
 import { SkjemaState } from '../../../model/skjema';
-import { fetcher as api } from '../../../lib/api-utils';
-import byggFullforRegistreringPayload from '../../../lib/bygg-fullfor-registrering-payload';
-import { FeilmeldingGenerell } from '../../feilmeldinger/feilmeldinger';
-import { FullforRegistreringResponse } from '../../../model/registrering';
-import hentKvitteringsUrl from '../../../lib/hent-kvitterings-url';
-import { loggAktivitet, loggFlyt } from '../../../lib/amplitude';
-import { hentRegistreringFeiletUrl } from '../../../lib/hent-registrering-feilet-url';
-import { OppgaveRegistreringstype } from '../../../model/feilsituasjonTyper';
 import FullforRegistreringKnappNyInngang from '../fullfor-registrering-knapp-ny-inngang';
 import SvarTabell from './SvarTabell';
 
@@ -53,111 +42,11 @@ const TEKSTER: Tekster<string> = {
     },
 };
 
-interface FullforProps {
-    skjemaState: SkjemaState;
-    onSubmit(): void;
-}
-
-const beregnTidBrukt = (skjemaState: SkjemaState) => {
-    if (!skjemaState.startTid) {
-        return;
-    }
-
-    return (Date.now() - skjemaState.startTid) / 1000;
-};
-
-const hentProfilering = async (response: FullforRegistreringResponse) => {
-    if (!response.type) {
-        try {
-            return await api('api/profilering/');
-        } catch (e) {
-            logger.error('profilering feilet', e);
-        }
-    }
-};
-
-interface FullforKnappProps extends FullforProps {
-    onValiderSkjema(): boolean;
-}
-
 interface OppsummeringProps {
     skjemaState: SkjemaState;
     skjemaPrefix: '/skjema/' | '/opplysninger/' | '/oppdater-opplysninger/';
     onSubmit(): void;
 }
-
-export const FullforRegistreringKnapp = (props: FullforKnappProps) => {
-    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
-    const [senderSkjema, settSenderSkjema] = useState<boolean>(false);
-    const [visFeilmelding, settVisFeilmelding] = useState<boolean>(false);
-    const [visFeilmeldingTeller, settVisFeilmeldingTeller] = useState<number>(0);
-    const router = useRouter();
-
-    const { skjemaState, onSubmit, onValiderSkjema } = props;
-
-    const validerOgFullfor = () => {
-        if (onValiderSkjema()) {
-            return fullforRegistrering();
-        }
-    };
-
-    const fullforRegistrering = useCallback(async () => {
-        try {
-            const body = byggFullforRegistreringPayload(skjemaState);
-            settSenderSkjema(true);
-            settVisFeilmelding(false);
-            onSubmit();
-
-            const response: FullforRegistreringResponse = await api(`api/fullforregistrering`, {
-                method: 'post',
-                body: JSON.stringify(body),
-            });
-
-            const feiltype = response.type;
-
-            if (feiltype) {
-                loggFlyt({ hendelse: 'Får ikke fullført registreringen', aarsak: feiltype });
-                return router.push(hentRegistreringFeiletUrl(feiltype, OppgaveRegistreringstype.REGISTRERING));
-            }
-
-            const profilering = await hentProfilering(response);
-
-            loggAktivitet({
-                aktivitet: 'Utfylling av skjema fullført',
-                tidBruktForAaFullforeSkjema: beregnTidBrukt(skjemaState),
-                innsatsgruppe: profilering ? profilering.innsatsgruppe : 'IKKE_PROFILERT',
-            });
-
-            loggFlyt({ hendelse: 'Sender inn skjema for registrering' });
-
-            return router.push(hentKvitteringsUrl());
-        } catch (e) {
-            settVisFeilmeldingTeller(visFeilmeldingTeller + 1);
-            settVisFeilmelding(true);
-            logger.error(e, `Registrering uten plikter feilet`);
-            if (visFeilmeldingTeller >= 3) {
-                return router.push('/feil/');
-            }
-        } finally {
-            settSenderSkjema(false);
-        }
-    }, [onSubmit, router, skjemaState, visFeilmeldingTeller]);
-
-    return (
-        <>
-            {visFeilmelding && (
-                <div className="mb-6">
-                    <FeilmeldingGenerell />
-                </div>
-            )}
-            <div style={{ textAlign: 'center' }}>
-                <Button onClick={validerOgFullfor} loading={senderSkjema} disabled={senderSkjema}>
-                    {tekst('fullfoerRegistrering')}
-                </Button>
-            </div>
-        </>
-    );
-};
 
 const OppsummeringUtenPlikter = (props: OppsummeringProps) => {
     const sprak = useSprak();
