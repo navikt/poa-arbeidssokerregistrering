dbfb13a7043f157d4178200b4aa51ce3b5412007
Geir Gåsodden <geir.gasodden@gmail.com>
2024-07-09 10:54:27 +0200

    r fjerner restene etter gamle kontaktinformasjon

--- src/pages/api/kontaktinformasjon.ts
+++
@@ -1,7 +0,0 @@
-import lagApiHandlerMedAuthHeaders from '../../lib/next-api-handler';
-import { withAuthenticatedApi } from '../../auth/withAuthentication';
-
-const kontaktinformasjonUrl = `${process.env.KONTAKTINFORMASJON_URL}`;
-const kontaktinformasjonHandler = lagApiHandlerMedAuthHeaders(kontaktinformasjonUrl);
-
-export default withAuthenticatedApi(kontaktinformasjonHandler);
