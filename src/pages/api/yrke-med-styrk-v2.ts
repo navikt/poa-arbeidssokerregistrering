83c9bf7512d682779d5b16666a9c3a2da6bbc775
Geir Gåsodden <geir.gasodden@gmail.com>
2024-09-26 12:29:38 +0200

    r legger til logging på feil mot PAM_ONTOLOGI

--- src/pages/api/yrke-med-styrk-v2.ts
+++ src/pages/api/yrke-med-styrk-v2.ts
@@ -8,6 +8,7 @@ async function yrkeMedStyrk(req: NextApiRequest, res: NextApiResponse<string>) {
     const callId = nanoid();
     const yrke = req.query.yrke;
     const url = `${process.env.PAM_ONTOLOGI_URL}/typeahead/stilling?stillingstittel=${yrke}`;
+
     const response = await fetch(url, {
         headers: {
             'Nav-Consumer-Id': 'poa-arbeidssokerregistrering',
@@ -15,10 +16,12 @@ async function yrkeMedStyrk(req: NextApiRequest, res: NextApiResponse<string>) {
             'Content-Type': 'application/json',
         },
     });
+
     if (!response.ok) {
         logger.error(`Respons fra PAM_ONTOLOGI typeahead ikke OK - [callId: ${callId}]`);
         res.status(500);
     }
+
     const json = await response.json();
     res.status(200).json(json);
 }
