02ff15cf3586a78c411e274ffa274f6c48ddf938
Jostein Holje <jostein.holje@nav.no>
2025-05-15 11:47:56 +0200

    Sett sidetittel på ny-kvittering

--- src/components/kvittering/ny-kvittering.tsx
+++ src/components/kvittering/ny-kvittering.tsx
@@ -8,9 +8,12 @@ import kvitteringIkonSvg from './kvittering-ikon.svg';
 import LenkePanel from '../lenke-panel';
 import { loggAktivitet } from '../../lib/amplitude';
 import Overskrift from '../skjema/overskrift';
+import React from 'react';
+import Head from 'next/head';
 
 const TEKSTER = {
     nb: {
+        sideTittel: 'Du er nå registrert som arbeidssøker',
         heading: 'Du er registrert som arbeidssøker',
         body1: 'Du må bekrefte at du ønsker å være registrert hver 14. dag.',
         body2: 'Nav vil vurdere de opplysningene du har gitt oss mot opplysningene vi har om andre arbeidssøkere i omtrent samme situasjon. Basert på dette vil en veileder fatte et oppfølgingsvedtak som sendes til deg. Vedtaket forteller hvordan Nav vurderer din situasjon i arbeidsmarkedet og hvilken hjelp du kan få fra Nav.',
@@ -26,6 +29,7 @@ const TEKSTER = {
         dagpengerLenkeBeskrivelse: 'Dette kan du ha rett til',
     },
     nn: {
+        sideTittel: 'Du er no registrert som arbeidssøkjar',
         heading: 'Du er registrert som arbeidssøkjar',
         body1: 'Du må stadfesta at du ønskjer å vera registrert kvar 14. dag.',
         body2: 'Nav vil vurdera dei opplysningane du har gitt oss mot opplysningane me har om andre arbeidssøkjarar i omtrent same situasjon. Basert på dette vil ein rettleiar gjera eit oppfølgingsvedtak som blir sendt til deg. Vedtaket fortel korleis Nav vurderer din situasjon i arbeidsmarknaden og kva hjelp du kan få frå Nav.',
@@ -41,6 +45,7 @@ const TEKSTER = {
         dagpengerLenkeBeskrivelse: 'Dette kan du ha rett til',
     },
     en: {
+        sideTittel: 'You are now registered as a jobseeker',
         heading: 'You are registered as a jobseeker',
         body1: 'To stay registered as a jobseeker with Nav, you must send a confirmation of this every 14 days.',
         body2: 'Nav will assess the information you have given us against the information we have about other job seekers in a similar situation. Based on this, a counselor will make a follow-up decision that will be sent to you. The decision will explain how Nav assesses your situation in the labor market and what help you can get from Nav.',
@@ -63,6 +68,9 @@ const NyKvittering = () => {
     const { dittNavUrl } = useConfig() as Config;
     return (
         <div className="max-w-4xl">
+            <Head>
+                <title>{tekst('sideTittel')}</title>
+            </Head>
             <HGrid columns={{ sm: 1, md: 1, lg: '1fr auto', xl: '1fr auto' }} gap={{ lg: 'space-24' }}>
                 <div style={{ width: '96px', height: '96px' }}>
                     <Image src={kvitteringIkonSvg} alt="ikon" width={96} height={96} />
