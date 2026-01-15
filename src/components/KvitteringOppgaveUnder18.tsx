725c981c44c39229ef65b8f4bbbce992ecc57209
Geir Gåsodden <geir.gasodden@gmail.com>
2026-01-15 09:28:33 +0100

    r oppdaterer stoppsiden for arbeidssøkere under 18 år

--- src/components/KvitteringOppgaveUnder18.tsx
+++ src/components/KvitteringOppgaveUnder18.tsx
@@ -1,5 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import virkedager from '@alheimsins/virkedager';
-import { Alert, AlertProps, BodyLong, BodyShort, GuidePanel, Heading, Link } from '@navikt/ds-react';
+import {
+    Alert,
+    AlertProps,
+    BodyLong,
+    BodyShort,
+    GuidePanel,
+    Heading,
+    Link,
+    HGrid,
+    GlobalAlert,
+} from '@navikt/ds-react';
 import { lagHentTekstForSprak, Tekster } from '@navikt/arbeidssokerregisteret-utils';
 
 import useSprak from '../hooks/useSprak';
@@ -7,9 +19,27 @@ import useSprak from '../hooks/useSprak';
 import { formaterDato } from '../lib/date-utils';
 import { Kontaktinformasjon } from './kontaktinformasjonUnder18';
 import { loggAktivitet } from '../lib/tracker';
+import LenkePanel from '../components/lenke-panel';
+import kvitteringIkonSvg from './kvittering-under18-ikon.svg';
 
 const TEKSTER: Tekster<string> = {
     nb: {
+        sideTittel: 'Du trenger samtykke for å registrere deg som arbeidssøker',
+        tittel: 'Du ønsker å registrere deg som arbeidssøker',
+        samtykkeLenke: 'https://www.nav.no/samtykke-foresatte',
+        samtykkeLenkeTittel: 'Samtykke fra foresatte',
+        samtykkeLenkeBeskrivelse: 'Les om hvorfor vi må ha samtykke og finn lenke til samtykkeskjema',
+        oppdatereOpplysningerLenke: 'https://www.nav.no/person/personopplysninger/nb/#kontaktinformasjon',
+        oppdatereOpplysningerLenkeTittel: 'Oppdater kontaktinformasjonen din',
+        oppdatereOpplysningerLenkeBeskrivelse: 'Se om kontaktinformasjonen vi har om deg er riktig',
+        kontaktOssLenke: 'https://www.nav.no/kontaktoss',
+        kontaktOssLenkeTittel: 'Kontakt oss',
+        kontaktOssLenkeBeskrivelse: 'Ta kontakt med Nav',
+        alertTittel: 'Du må ha samtykke for å registrere deg',
+        alertContent1:
+            'Du er under 18 år og trenger samtykke fra foresatte for å kunne registrere deg som arbeidssøker',
+        alertContent2: 'En veileder hos oss vil kontakte deg innen utgangen av',
+        alertContent3: 'Veilederen vil hjelpe deg videre med samtykke og registrering',
         alertVennligstVent: 'Vennligst vent',
         alleredeBedtOmKontakt:
             'Du har allerede bedt oss kontakte deg. Vi tar kontakt i løpet av to arbeidsdager regnet fra den første meldingen. Pass på at kontaktopplysningene dine er oppdatert ellers kan vi ikke nå deg.',
@@ -84,42 +114,78 @@ export const KvitteringOppgaveOpprettet = () => {
     const idag = new Date();
     const toVirkedagerFraNaa = formaterDato(virkedager(idag, 2));
 
-    const gaarTilNavno = () => {
-        loggAktivitet({ aktivitet: 'Går til Samtykke fra foresatte' });
-    };
-
-    const gaarTilKontaktOss = () => {
-        loggAktivitet({ aktivitet: 'Går til kontakt oss', komponent: 'KvitteringOppgaveUnder18' });
-    };
-
     return (
-        <GuidePanel poster>
-            <Alert variant="success" className={'mb-6'}>
-                <BodyShort spacing>{tekst('henvendelseMottatt')}</BodyShort>
-                <BodyLong spacing>{tekst('trengerSamtykke')}</BodyLong>
-                <BodyLong spacing>
-                    {tekst('veilederKontakterDeg')} {toVirkedagerFraNaa}.
-                </BodyLong>
-                <BodyLong spacing>{tekst('veilederenHjelperDeg')}</BodyLong>
-                <BodyLong spacing>
-                    {tekst('lesMerOmSamtykkeIntro')}{' '}
-                    <Link href="https://www.nav.no/samtykke-foresatte" onClick={gaarTilNavno}>
-                        {tekst('samtykkeIntroLenkeTekst')}
-                    </Link>
-                    .
-                </BodyLong>
-            </Alert>
-            <div className="mb-8">
-                <Kontaktinformasjon />
-            </div>
-            <BodyLong spacing>
-                {tekst('hvisDuIkkeVilRegistreres')}{' '}
-                <Link href="https://www.nav.no/kontaktoss" onClick={gaarTilKontaktOss}>
-                    {tekst('taKontaktMedOss')}
-                </Link>
-                .
-            </BodyLong>
-        </GuidePanel>
+        <div className="max-w-4xl">
+            <Head>
+                <title>{tekst('sideTittel')}</title>
+            </Head>
+            <HGrid columns={{ sm: 1, md: 1, lg: '1fr auto', xl: '1fr auto' }} gap={{ lg: 'space-24' }}>
+                <div style={{ width: '96px', height: '96px' }}>
+                    <Image src={kvitteringIkonSvg} alt="ikon" width={96} height={96} />
+                </div>
+                <div>
+                    <Heading size={'xlarge'} level={'1'} spacing>
+                        {tekst('tittel')}
+                    </Heading>
+                    <GlobalAlert status="warning">
+                        <GlobalAlert.Header>
+                            <GlobalAlert.Title>{tekst('alertTittel')}</GlobalAlert.Title>
+                        </GlobalAlert.Header>
+                        <GlobalAlert.Content>
+                            <BodyLong spacing>{tekst('alertContent1')}</BodyLong>
+                            <BodyLong spacing>
+                                {tekst('alertContent2')} {toVirkedagerFraNaa} {tekst('alertContent3')}
+                            </BodyLong>
+                        </GlobalAlert.Content>
+                    </GlobalAlert>
+                    <BodyShort spacing>{tekst('body1')}</BodyShort>
+                    <BodyLong spacing>{tekst('body2')}</BodyLong>
+                    <Heading spacing size={'small'} level={'3'}>
+                        {tekst('innholdHeading')}
+                    </Heading>
+                    <ul className={'list-none'}>
+                        <li className={'mb-4'}>
+                            <LenkePanel
+                                href={tekst('samtykkeLenke')}
+                                title={tekst('samtykkeLenkeTittel')}
+                                description={tekst('samtykkeLenkeBeskrivelse')}
+                                onClick={() =>
+                                    loggAktivitet({
+                                        aktivitet: 'Går til Samtykke fra foresatte',
+                                    })
+                                }
+                            />
+                        </li>
+                        <li className={'mb-4'}>
+                            <LenkePanel
+                                href={tekst('oppdatereOpplysningerLenke')}
+                                title={tekst('oppdatereOpplysningerLenkeTittel')}
+                                description={tekst('oppdatereOpplysningerLenkeBeskrivelse')}
+                                onClick={() =>
+                                    loggAktivitet({
+                                        aktivitet: 'Går til endre personopplysninger',
+                                        komponent: 'KvitteringOppgaveUnder18',
+                                    })
+                                }
+                            />
+                        </li>
+                        <li className={'mb-4'}>
+                            <LenkePanel
+                                href={tekst('kontaktOssLenke')}
+                                title={tekst('kontaktOssLenkeTittel')}
+                                description={tekst('kontaktOssLenkeBeskrivelse')}
+                                onClick={() =>
+                                    loggAktivitet({
+                                        aktivitet: 'Går til endre personopplysninger',
+                                        komponent: 'KvitteringOppgaveUnder18',
+                                    })
+                                }
+                            />
+                        </li>
+                    </ul>
+                </div>
+            </HGrid>
+        </div>
     );
 };
 
