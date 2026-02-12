e55d9165bf5bb00388256b43cfe3e60be33cb814
Jostein Holje <jostein.holje@nav.no>
2026-02-12 14:52:00 +0100

    refactoring

--- src/app/api/oppgave-under-18/route.ts
+++ src/app/api/oppgave-under-18/route.ts
@@ -1,17 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { AIA_BACKEND_CLIENT_ID, proxyRequestWithAuth } from '@/lib/next-api-handler';
 
-const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
 const opprettOppgaveUrl = `${process.env.OPPGAVE_API_URL}/under-18`;
 
-export async function POST(req: NextRequest) {
-    if (brukerMock) {
-        return NextResponse.json({
-            id: 0,
-            tildeltEnhetsnr: 'string',
-            oppgaveType: 'UNDER_18',
-        });
-    }
-
-    return proxyRequestWithAuth(req, opprettOppgaveUrl, AIA_BACKEND_CLIENT_ID, 'POST');
-}
+export const POST = proxyRequestWithAuth(opprettOppgaveUrl, AIA_BACKEND_CLIENT_ID, 'POST', () =>
+    NextResponse.json({
+        id: 0,
+        tildeltEnhetsnr: 'string',
+        oppgaveType: 'UNDER_18',
+    }),
+);
--- src/lib/next-api-handler.ts
+++ src/lib/next-api-handler.ts
@@ -1,4 +1,4 @@
-import { NextApiHandler, NextApiRequest } from 'next';
+import { NextApiRequest } from 'next';
 import { nanoid } from 'nanoid';
 import { logger } from '@navikt/next-logger';
 import { getToken, requestTokenxOboToken } from '@navikt/oasis';
@@ -11,18 +11,6 @@ export const getHeaders = (token: string, callId: string) => {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
     };
-};
-
-export const lagApiPostHandlerMedAuthHeaders: (
-    url: string,
-    errorHandler: ((response: Response) => void) | undefined,
-    clientId: ClientIds,
-) => NextApiHandler = (url: string, errorHandler, clientId) => async (req, res) => {
-    if (req.method === 'POST') {
-        return lagApiHandlerMedAuthHeaders(url, errorHandler, clientId)(req, res);
-    } else {
-        res.status(405).end();
-    }
 };
 
 export interface ApiError extends Error {
@@ -41,6 +29,58 @@ export type ClientIds =
     | typeof INNGANG_CLIENT_ID
     | typeof OPPSLAG_V2_CLIENT_ID;
 
+const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
+export function proxyRequestWithAuth(
+    url: string,
+    clientId: ClientIds,
+    method: 'GET' | 'POST' = 'POST',
+    mockResponse: () => NextResponse,
+) {
+    return async function (req: NextRequest): Promise<NextResponse> {
+        if (brukerMock) {
+            return mockResponse();
+        }
+        const callId = nanoid();
+
+        try {
+            const body = method === 'POST' ? await req.json() : null;
+            const idPortenToken = getToken(req)!;
+            const tokenResult = await requestTokenxOboToken(idPortenToken, clientId);
+
+            if (!tokenResult.ok) {
+                throw tokenResult.error;
+            }
+
+            logger.info(`Starter kall callId: ${callId} mot ${url}`);
+
+            const response = await fetch(url, {
+                method,
+                body: body ? JSON.stringify(body) : undefined,
+                headers: getHeaders(tokenResult.token, callId),
+            });
+
+            const contentType = response.headers.get('content-type');
+
+            if (!response.ok) {
+                logger.error(`apiResponse ikke ok, contentType: ${contentType}, callId - ${callId}`);
+                return new NextResponse(`Noe gikk galt (callId: ${callId})`, { status: response.status });
+            }
+
+            logger.info(`Kall callId: ${callId} mot ${url} er ferdig`);
+
+            if (contentType?.includes('application/json')) {
+                const json = await response.json();
+                return NextResponse.json(json);
+            } else {
+                return new NextResponse(response.body);
+            }
+        } catch (error) {
+            logger.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
+            return new NextResponse(`Noe gikk galt (callId: ${callId})`, { status: 500 });
+        }
+    };
+}
+
 const exchangeIDPortenToken = async (clientId: string, idPortenToken: string): Promise<string> => {
     const result = await requestTokenxOboToken(idPortenToken, clientId);
     if (!result.ok) {
@@ -50,110 +90,6 @@ const exchangeIDPortenToken = async (clientId: string, idPortenToken: string): P
     return result.token;
 };
 
-export async function proxyRequestWithAuth(
-    req: NextRequest,
-    url: string,
-    clientId: ClientIds,
-    method: 'GET' | 'POST' = 'POST',
-): Promise<NextResponse> {
-    const callId = nanoid();
-    const body = method === 'POST' ? await req.json() : null;
-
-    try {
-        const idPortenToken = getToken(req)!;
-        const tokenResult = await requestTokenxOboToken(idPortenToken, clientId);
-
-        if (!tokenResult.ok) {
-            throw tokenResult.error;
-        }
-
-        logger.info(`Starter kall callId: ${callId} mot ${url}`);
-
-        const response = await fetch(url, {
-            method,
-            body: body ? JSON.stringify(body) : undefined,
-            headers: getHeaders(tokenResult.token, callId),
-        });
-
-        const contentType = response.headers.get('content-type');
-
-        if (!response.ok) {
-            logger.error(`apiResponse ikke ok, contentType: ${contentType}, callId - ${callId}`);
-            return new NextResponse(`Noe gikk galt (callId: ${callId})`, { status: response.status });
-        }
-
-        logger.info(`Kall callId: ${callId} mot ${url} er ferdig`);
-
-        if (contentType?.includes('application/json')) {
-            const json = await response.json();
-            return NextResponse.json(json);
-        } else {
-            return new NextResponse(response.body);
-        }
-    } catch (error) {
-        logger.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
-        return new NextResponse(`Noe gikk galt (callId: ${callId})`, { status: 500 });
-    }
-}
-
-export const getTokenFromRequest = getToken;
-
-const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
-
 export const getAaregToken = async (req: NextRequest | NextApiRequest) => {
-    return getTokenXToken(req, AAREG_CLIENT_ID);
-};
-
-export const getInngangClientId = async (req: NextApiRequest) => {
-    return getTokenXToken(req, INNGANG_CLIENT_ID);
-};
-
-const getTokenXToken = async (req: NextApiRequest | NextRequest, clientId: ClientIds) => {
-    return await exchangeIDPortenToken(clientId, getTokenFromRequest(req)!);
-};
-
-const lagApiHandlerMedAuthHeaders: (
-    url: string,
-    errorHandler: ((response: Response) => void) | undefined,
-    clientId: ClientIds,
-) => NextApiHandler = (url: string, errorHandler, clientId) => async (req, res) => {
-    const callId = nanoid();
-    let body = null;
-
-    if (req.method === 'POST') {
-        body = req.body;
-    }
-
-    try {
-        logger.info(`Starter kall callId: ${callId} mot ${url}`);
-        const response = await fetch(url, {
-            method: req.method,
-            body,
-            headers: brukerMock ? getHeaders('token', callId) : getHeaders(await getTokenXToken(req, clientId), callId),
-        }).then(async (apiResponse) => {
-            const contentType = apiResponse.headers.get('content-type');
-
-            if (!apiResponse.ok) {
-                logger.error(`apiResponse ikke ok, contentType: ${contentType}, callId - ${callId}`);
-                if (typeof errorHandler === 'function') {
-                    return errorHandler(apiResponse);
-                } else {
-                    const error = new Error(apiResponse.statusText) as ApiError;
-                    error.status = apiResponse.status;
-                    throw error;
-                }
-            }
-
-            if (contentType?.includes('application/json')) {
-                return apiResponse.json();
-            } else {
-                return apiResponse;
-            }
-        });
-        logger.info(`Kall callId: ${callId} mot ${url} er ferdig`);
-        return res.json(response);
-    } catch (error) {
-        logger.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
-        res.status((error as ApiError).status || 500).end(`Noe gikk galt (callId: ${callId})`);
-    }
+    return await exchangeIDPortenToken(AAREG_CLIENT_ID, getToken(req)!);
 };
