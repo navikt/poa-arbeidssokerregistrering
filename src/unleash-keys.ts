3d3d472e75cd9323ecf8a6ebe338743b7c628652
Jostein Holje <jostein.holje@nav.no>
2026-03-05 14:42:20 +0100

    Fjern styrkløft fra unleash keys

--- src/unleash-keys.ts
+++ src/unleash-keys.ts
@@ -1,7 +1,6 @@
 const unleashKeys = {
     BRUK_UXSIGNALS: 'arbeidssoekerregisteret.bruk-uxsignals',
     BRUK_SKYRA: 'arbeidssoekerregisteret.bruk-skyra',
-    VIS_STYRKELOEFT: 'eksperiment.styrkeloft',
     BRUK_OPPGAVE_FRA_HENDELSE: 'arbeidssoekerregisteret.bruk-oppgave-fra-hendelse',
 };
 
