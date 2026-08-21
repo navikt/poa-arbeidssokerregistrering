12fe2eb9a5d94e09a78ddba49419bc1092729f81
Geir Gåsodden <geir.gasodden@gmail.com>
2026-08-21 06:44:18 +0200

    build: eksluderer .next fra vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
         environment: 'jsdom',
         globals: true,
         setupFiles: './setup-tests.ts',
-        exclude: ['node_modules/**', 'e2e/**'],
+        exclude: ['node_modules/**', 'e2e/**', '.next'],
     },
 });
